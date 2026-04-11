6fff4fbd012cbf733ae6876c669db07984fbb898
Pooja Gadige <gadigep@mail.gvsu.edu>
2026-04-10 20:21:09 -0400

    Add Firebase configuration

--- src/firebase.ts
+++ src/firebase.ts
@@ -9,6 +9,12 @@ const firebaseConfig = {
   //   projectId: "your-project-name-here",
   //   storageBucket: "your-project-name.appspot.com",
   //   messagingSenderId: "xxxxxxxx",
+  apiKey: "AIzaSyAGVzfSDSEKNOKDZLc9mtrumIfnPmiM0QE",
+  authDomain: "beverageshop-14060.firebaseapp.com",
+  projectId: "beverageshop-14060",
+  storageBucket: "beverageshop-14060.firebasestorage.app",
+  messagingSenderId: "587237184942",
+  appId: "1:587237184942:web:33ea55cefb7bc770c8e43f"
 };
 
 const app = initializeApp(firebaseConfig);
