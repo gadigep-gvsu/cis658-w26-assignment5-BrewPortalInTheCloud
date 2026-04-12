1f5a3b3cd537fb1ac42b11cb1a81f85a12b704c4
Pooja Gadige <gadigep@mail.gvsu.edu>
2026-04-12 17:35:30 -0400

    Refactor Firebase configuration code

---
+++ src/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAGVzfSDSEKNOKDZLc9mtrumIfnPmiM0QE",
+  authDomain: "beverageshop-14060.firebaseapp.com",
+  projectId: "beverageshop-14060",
+  storageBucket: "beverageshop-14060.firebasestorage.app",
+  messagingSenderId: "587237184942",
+  appId: "1:587237184942:web:33ea55cefb7bc770c8e43f",
+};
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,25 +1,10 @@
-import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
-import { GoogleAuthProvider } from "firebase/auth";
-
-const firebaseConfig = {
-  //   // COPY this from your Firebase Console
-  //   apiKey: "your-api-key-goes-here",
-  //   authDomain: "your-project-name-here.firebaseapp.com",
-  //   databaseURL: "https://your-project-name-here.firebaseio.com",
-  //   projectId: "your-project-name-here",
-  //   storageBucket: "your-project-name.appspot.com",
-  //   messagingSenderId: "xxxxxxxx",
-  apiKey: "AIzaSyAGVzfSDSEKNOKDZLc9mtrumIfnPmiM0QE",
-  authDomain: "beverageshop-14060.firebaseapp.com",
-  projectId: "beverageshop-14060",
-  storageBucket: "beverageshop-14060.firebasestorage.app",
-  messagingSenderId: "587237184942",
-  appId: "1:587237184942:web:33ea55cefb7bc770c8e43f"
-};
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { firebaseConfig } from "./firebase.config";
 
 const app = initializeApp(firebaseConfig);
-const provider = new GoogleAuthProvider();
-const db = getFirestore(app);
 
-export default db;
+export { firebaseConfig };
+export const db = getFirestore(app);
+export const auth = getAuth(app);
